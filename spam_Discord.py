0982c65887aae5d96eb2b6a26cc08a6ab39d4cb7
kenzn2 <77497884+kenzn2@users.noreply.github.com>
2022-07-22 11:47:11 +0700

    Add files via upload

---
+++ spam_Discord.py
@@ -0,0 +1,34 @@
+import requests
+import time,random
+
+def array_gen():
+    global array
+    for arr in range(10):
+        array.append(int(random.randint(0, 100)))
+
+
+author = input("authorization : ")
+channelID = input("channelID : ")
+n = int(input("How many times do you want to spam ? : "))
+array = []
+temp_array = []
+
+str1 = 'https://discord.com/api/v9/channels/'
+str2 = '/messages'
+str_all = str1 +channelID+ str2
+
+
+header = {
+    'authorization': author
+}
+
+for i in range(n):
+    array_gen()
+    temp_array = str(array)
+    payload = {
+    'content':temp_array
+    }
+    
+    r = requests.post(str_all, data=payload, headers=header)
+    time.sleep(random.randint(1, 5))
+    array = []
